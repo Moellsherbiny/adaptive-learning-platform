dd4f47d5c6c520f3273ac12f01ccf7fb76097992
maibadran <elcendrella77@gmail.com>
2025-11-07 23:19:52 +0200

    deploy settings

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
   env: {
     NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
   },
