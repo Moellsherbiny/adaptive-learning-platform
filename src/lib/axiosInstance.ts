70f16555d9a8115396306c4066ab3c61979f49bc
Mohamed Elsherbiny <moellsherbiny@gmail.com>
2025-11-09 10:35:03 +0200

    fix: base url

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL =
   process.env.NEXT_PUBLIC_BASE_URL ||
-  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000");
+  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "https://adaptive-learning-platform-eight.vercel.app");
 
 export const axiosInstance = axios.create({
   baseURL: `${BASE_URL}/api`,
