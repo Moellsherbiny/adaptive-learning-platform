ad3056fea32da025baba8ce1a558f5b06997f48e
Mohamed Elsherbiny <moellsherbiny@gmail.com>
2025-11-09 09:58:53 +0200

    update axios instance to use dynamic base URL from environment variable

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
+const BASEURL = process.env.BASE_URL;
 export const axiosInstance = axios.create({
-  baseURL: `http://localhost:3000/api`,
+  baseURL: `${BASEURL}/api`,
   withCredentials: true,
 });
